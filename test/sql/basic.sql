150e27457c21a6a5e4d6e03e9d357e5c8d7a656d
Jeremy Schneider <schneider@ardentperf.com>
2023-09-17 00:11:42 +0000

    fix pg13 test and re-enable. needed small sleep for stats to update.

--- test/sql/basic.sql
+++ test/sql/basic.sql
@@ -8,6 +8,7 @@ select ds_start();
 
 select regexp_replace("QUERY PLAN",'Version:.*','Version') from explain_analyze_full($$ select max(a) from dsef1 $$) limit 1;; 
 
+select pg_sleep(0.5);
 set client_min_messages=warning;
 select distinct scope from ds_report() where scope in ('Database','Instance');
 
@@ -21,5 +22,6 @@ select ds_capture();
 
 select regexp_replace("QUERY PLAN",'Version:.*','Version') from explain_analyze_full($$ select min(a) from dsef1 $$) limit 1;; 
 
+select pg_sleep(0.5);
 set client_min_messages=warning;
 select distinct scope from ds_report_diff() where scope in ('Database','Instance');
